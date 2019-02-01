8305deef25c9c27f0d81202e2328f855625387a0
unknown <VAU0610@ad.uniandes.edu.co>
2019-02-01 13:51:56 -0500

    Fin de laboratorio

---
+++ src/model/data_structures/ListaEncadenada.java
@@ -0,0 +1,10 @@
+package model.data_structures;
+
+/**
+ *  API de listas encadenadas a utilizar, el generics de esta lista es Node.
+ * Esta clase se encarga del almacenamiento de la información.
+ */
+public class ListaEncadenada<Nodo> {
+	
+
+}
---
+++ src/model/data_structures/Node.java
@@ -0,0 +1,15 @@
+package model.data_structures;
+
+/**
+ * Clase base para especificación de lista encadenada (generics).
+ * Esta clase se encarga del almacenamiento de la información por nodo.
+ */
+public class Node implements Comparable {
+
+	
+	public int compareTo(Object arg0) {
+		
+		return 0;
+	}
+
+}
